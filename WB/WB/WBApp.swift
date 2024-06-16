37893ca956fb4c5f3b9ec04574b8b29b9717278e
RomStark <rgtarkovskiy@edu.hse.ru>
2024-06-17 01:03:43 +0300

    initial commit

---
+++ WB/WB/WBApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBApp.swift
+//  WB
+//
+//  Created by Al Stark on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
